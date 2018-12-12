8ec18cc49672f18fdd947b4015c6253ec6b2e413
Murilo Costa <murilo.nunes.jose@gmail.com>
2018-12-12 00:06:03 -0200

    Fix #6

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Middleware;
+
+class Cors
+{
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @param  string|null $guard
+     * @return mixed
+     */
+    public function handle($request, \Closure $next, $guard = null)
+    {
+        $headers = [
+            'Access-Control-Allow-Origin' => '*',
+            'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, DELETE',
+            'Access-Control-Allow-Headers' => $request->header('Access-Control-Request-Headers')
+        ];
+
+        if ($request->isMethod('OPTIONS')) {
+            return response(null, 200, $headers);
+        }
+
+        $response = $next($request);
+        foreach ($headers as $key => $value) {
+            $response->header($key, $value);
+        }
+
+        return $response;
+    }
+
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -58,9 +58,9 @@ $app->singleton(
 |
 */
 
-// $app->middleware([
-//    App\Http\Middleware\ExampleMiddleware::class
-// ]);
+$app->middleware([
+    App\Http\Middleware\Cors::class
+]);
 
 // $app->routeMiddleware([
 //     'auth' => App\Http\Middleware\Authenticate::class,
